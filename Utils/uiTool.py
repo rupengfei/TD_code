9c495a08814fe96303009d5a8fba53491396ee8a
rupengfei <a773849069@gmail.com>
2018-12-04 16:20:07 +0800

    add DH_referenceManager Tool add uiTool add Qt.py

--- Utils/uiTool.py
+++ Utils/uiTool.py
@@ -5,15 +5,43 @@
 #         time: 2018/12/3
 # ==========================================
 import maya.OpenMayaUI as OpenMayaUI
-import shiboken2
+from cStringIO import StringIO
 from PySide2 import QtWidgets
+import xml.etree.ElementTree as xml
+import Utils.Qt as Qt
+
+if Qt.__binding__ == 'PySide':
+    from shiboken import wrapInstance
+    import pysideuic as uic
+elif Qt.__binding__ == 'PySide2':
+    from shiboken2 import wrapInstance
+    import pyside2uic as uic
 # --*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+
+
+def loadUiType(uiFile):
+    parsed = xml.parse(uiFile)
+    widget_class = parsed.find('widget').get('class')
+    form_class = parsed.find('class').text
+
+    with open(uiFile, 'r') as f:
+        o = StringIO()
+        frame = {}
+
+        uic.compileUi(f, o, indent=0)
+        pyc = compile(o.getvalue(), '<string>', 'exec')
+        exec pyc in frame
+
+        # Fetch the base_class and form class based on their type in the xml from designer
+        form_class = frame['Ui_%s' % form_class]
+        base_class = getattr(QtWidgets, widget_class)
+        return form_class, base_class
 
 
 def get_maya_window():
     # 获取maya主界面窗口名称
     window = OpenMayaUI.MQtUtil.mainWindow()
-    return shiboken2.wrapInstance(long(window), QtWidgets.QWidget)
+    return wrapInstance(long(window), QtWidgets.QWidget)
 
 
 def windowExists(parent, name):
