3b6719e71b9202e86c70ed045b0ecb30f5ca3f80
rupengfei <a773849069@gmail.com>
2018-12-05 13:28:44 +0800

    修改shaderIO层级

--- core/test/qt_test1.py
+++ core/test/qt_test1.py
@@ -4,27 +4,22 @@
 #         mail: a773849069@gmail.com
 #         time: 2018/12/5
 # ==========================================
-import sys
-from PySide2 import QtCore, QtGui, QtWidgets
+# import sys
+# from PySide2 import QtCore, QtGui, QtWidgets
 import Utils.uiTool as uiTool
-
+import Utils.scriptTool as scriptTool
 # --*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
-
-form_class, base_class = uiTool.loadUiType("D:/___________TD____________/TD_Code/core/test/qt_test1_ui.ui")
-
+script_path = scriptTool.getScriptPath()
+form_class, base_class = uiTool.loadUiType(script_path + "/qt_test1_ui.ui")
 
 
 class ShaderIO(base_class, form_class):
-    '''
-    '''
     def __init__(self, parent=uiTool.get_maya_window()):
+        self.window_name = 'qt_test_ui'
         super(ShaderIO, self).__init__(parent)
         self.setupUi(self)
+        # self.__current_dir = ''
 
-        self.__current_dir = ''
+    def show_win(self):
+        uiTool.windowExists(uiTool.get_maya_window(), "qt_test_ui")
 
-        self.show()
-
-
-# if __name__ == "__main__":
-#     ShaderIO()
