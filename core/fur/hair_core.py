13493c29bd0863f78f4f957d3c369a803ea213fc
rupengfei <a773849069@gmail.com>
2019-01-26 12:04:06 +0800

    毛发找回

---
+++ core/fur/hair_core.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-  
+# ==========================================
+#       author: Pengfei.Ru
+#         mail: a773849069@gmail.com
+#         time: 2019/1/26
+# ==========================================
+from Utils import mayaTool, config
+import json
+import maya.cmds as mc
+# --*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+
+
+def import_fur_BS():
+    with open(config.seer7_fur, "r") as f:
+        data = json.load(f)
+    for ia, ib in data.iteritems():  # 循环角色
+        list1 = mc.ls(ib["geo_name"])
+        list1.extend(mc.ls("*:" + ib["geo_name"]))
+        if not list1:
+            continue
+        for num, geo_name in enumerate(list1):  # 循环重复角色
+            fix = ("{%d}" % num) if num else ""
+            name_space = mc.file(ib["fur_file_path"] + fix, query=True, namespace=True)
+            # if name_space != "unknown":
+            #     pass
+            if name_space == "unknown":
+                mayaTool.reference_file(ib["fur_file_path"], name_space=ia, typ="ma")
+                # 为避免文件路径后缀查找出错 {1}，再此处不用mayaTool返回的namespace
+                name_space = mc.file(ib["fur_file_path"] + fix, query=True, namespace=True)
+
+                bs_a = geo_name
+                bs_b = name_space + ":" + ib["fur_name"]
+                # blendShape A 动 B 静
+                mc.blendShape([bs_a, bs_b], before=True, w=[0, 1], origin="world")
